46b4683dd8bcde74d9e58465abf5b58e03a2d7a7
Praveen Jhawar <nfspop@gmail.com>
2015-12-21 23:37:50 +0530

    ADD PUT /todos/:id

--- server.js
+++ server.js
@@ -88,6 +88,39 @@ app.delete('/todos/:id', function (req, res) {
 	res.json(matchedTODO);
 });
 
+app.put('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTODO = _.findWhere(todos, {id: todoID});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTODO)
+	{
+		return res.status(404).json({"error": "no todo found with this todo"});
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+	{
+		validAttributes.completed = body.completed;
+	}
+	else if(body.hasOwnProperty('completed'))
+	{
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+	{
+		validAttributes.description = body.description;
+	}
+	else if(body.hasOwnProperty('description'))
+	{
+		return res.status(404).send();
+	}
+
+	_.extend(matchedTODO, validAttributes);
+	res.json(matchedTODO);
+});
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
