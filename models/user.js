cbff03f629aeb54cae4666e285007ffacad89dac
Praveen Jhawar <nfspop@gmail.com>
2015-12-24 03:10:39 +0530

    Add auth missleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	//return sequelize.define('user', {
-	var user = sequelize.define('user', {	
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -65,6 +65,27 @@ module.exports = function(sequelize, DataTypes) {
 						reject();
 					})
 				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function(user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function(error) {
+							reject();
+						});
+					} catch (error) {
+						reject();
+					}
+				});
 			}
 		},
 		instanceMethods: {
@@ -73,12 +94,15 @@ module.exports = function(sequelize, DataTypes) {
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
 			generateToken: function(type) {
-				if(!_.isString(type)) {
+				if (!_.isString(type)) {
 					return undefined;
 				}
 
 				try {
-					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
 					var token = jwt.sign({
 						token: encryptedData
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.get('/', function(req, res) {
 	res.send('TODO API root');
 });
 
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -42,7 +43,7 @@ app.get('/todos', function(req, res) {
 	});
 });
 
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoID).then(function(todo) {
@@ -58,7 +59,7 @@ app.get('/todos/:id', function(req, res) {
 	});
 });
 
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	//console.log("aya " + db.hasOwnProperty('todo'));
@@ -70,7 +71,7 @@ app.post('/todos', function(req, res) {
 	});
 });
 
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -91,7 +92,7 @@ app.delete('/todos/:id', function(req, res) {
 	});
 });
 
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
 
 	var body = _.pick(req.body, 'description', 'completed');
@@ -143,7 +144,7 @@ app.post('/users/login', function(req, res) {
 		var token = user.generateToken('authentication');
 
 		if (token) {
-			res.header('Auth', ).json(user.toPublicJSON());
+			res.header('Auth', token).json(user.toPublicJSON());
 		} else {
 			res.status(401).send();
 		}
