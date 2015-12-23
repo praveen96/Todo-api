da28a04b4e7985d6b953dcffb38de2fcab673503
Praveen Jhawar <nfspop@gmail.com>
2015-12-24 00:04:19 +0530

    Add login route and authenticate class method.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -128,14 +129,45 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.user.create(body).then(function (user) {
+	db.user.create(body).then(function(user) {
 		res.json(user.toPublicJSON());
 	}, function(error) {
 		res.status(400).json(error);
 	});
 });
 
-db.sequelize.sync().then(function() {
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function(error) {
+		res.status(401).send();
+	});
+
+	/*if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return res.status(400).send();
+	}
+
+	db.user.findOne({
+		where: {
+			email: body.email
+		}
+	}).then(function(user) {
+		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+			// 401 -> authentication is possible but failed
+			return res.status(401).send();
+		}
+
+		res.json(user.toPublicJSON());
+	}).then(function(error) {
+		res.status(500).send();
+	})*/
+
+	//res.json(body);
+});
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
